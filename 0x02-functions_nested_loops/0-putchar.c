6ba686cc33e454ee10ddfa1a2c034209de6851b7
tosbery <toladunjoye2@gmail.com>
2022-09-13 14:04:41 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/*
+ *
+ * main - prints _putchar
+ *
+ * @void: Empty parameter list for main.
+ *
+ * Description:
+ *
+ *
+ */
+
+int main(void)
+{
+
+_putchar ("_putchar");
+_putchar ('\n');
+return (0);
+
+}
+
