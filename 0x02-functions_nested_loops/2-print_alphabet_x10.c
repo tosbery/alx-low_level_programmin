c30d5bc55daefe4613e50e173cfb3da888b4c5d5
tosbery <toladunjoye2@gmail.com>
2022-09-14 10:01:56 -0700

    Print Alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/*
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+void print_alphabet(void)
+{
+
+char i;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+}
+
+/*
+ * print_alphabet_x10 - prints aphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+
+int i;
+
+for (i = '0'; i <= '9'; i++)
+{
+print_alphabet();
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,3 +7,7 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 int add(int, int);
+void jack_bauer(void);
+void times_table(void);
+void print_to_98(int);
+void print_times_table(int n);
